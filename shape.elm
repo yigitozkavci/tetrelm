ad5cf3c655ed1d06b0ab2aa98e4ddb9dc276a22a
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2016-12-31 04:48:10 +0300

    Let shapes flow randomly

--- shape.elm
+++ shape.elm
@@ -6,10 +6,16 @@ import Board exposing (Board, board)
 import Grid
 import Block exposing (Block)
 
-type Shape = L | RL | I | S
+type ShapeType = L | RL | I | S | T
+
+type alias Shape =
+  { x : Int
+  , y : Int
+  , shapeType : ShapeType
+  }
 
 
-blockShapeLocations : Location -> Shape -> List Location
+blockShapeLocations : Location -> ShapeType -> List Location
 blockShapeLocations startingLocation shape =
   case shape of
     L ->
@@ -40,16 +46,21 @@ blockShapeLocations startingLocation shape =
         , (0, 1)
         , (1, 1)
         ]
+    T ->
+      List.map (Location.shiftLocationBy startingLocation)
+        [ (0, 0)
+        , (1, 0)
+        , (2, 0)
+        , (1, 1)
+        ]
+
+
+shapeToForm : Shape -> Form
+shapeToForm shape =
+  List.map generateBlock (blockShapeLocations (shape.x, shape.y) shape.shapeType) |> group
 
 
 generateBlock : Location -> Block
 generateBlock location =
   filled red (square (toFloat board.tileSize))
     |> move (Grid.toFloatPos (Location.scaleLocation board.tileSize location))
-
-
-generateShape : Location -> Shape -> Form
-generateShape location shape =
-  List.map (\location -> generateBlock location)
-    (blockShapeLocations location shape) |> group
-
--- tetris.elm
+++ tetris.elm
@@ -1,12 +1,15 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, input, div)
+import Html.Attributes exposing (placeholder)
+import Html.Events exposing (onInput)
 import Element exposing (toHtml)
 import Text
 import Color exposing(red, blue)
 import Grid
 import Board exposing (Board, board)
 import Location exposing (Location)
-import Shape exposing (Shape(..))
+import Shape exposing (Shape, shapeToForm, ShapeType(..))
+import Time exposing (Time, millisecond)
+import Random
 import Collage exposing
   ( Form
   , collage
@@ -15,32 +18,91 @@ import Collage exposing
   )
 
 main =
-  Html.beginnerProgram { model = board, view = view, update = update }
+  Html.program
+    { init = init, update = update, subscriptions = subscriptions, view = view }
+
+type alias Model =
+  { shapes : List Shape
+  , pieceInterval : Int
+  }
 
 
-type Msg = Increment
+model : Model
+model =
+  { shapes =
+      [ { x = 3, y = 4, shapeType = L }
+      ]
+  , pieceInterval = 1
+  }
+
+
+init =
+  (model, Cmd.none)
+
+
+type Msg = Tick Time | EmptyMsg | RandomXPos Int | RandomType Int Int
+
+
+dropOnePixel : Shape -> Shape
+dropOnePixel shape =
+  { shape | y = shape.y + 1 }
+
+
+-- decideCmdByTime : Model -> Cmd Msg
+-- decideCmdByTime model =
+--   if model.time > 10 then (SendNewPiece) else Cmd.none
+
+
+decodeShapeType : Int -> ShapeType
+decodeShapeType encodedType =
+  case encodedType of
+    1 -> L
+    2 -> RL
+    3 -> I
+    4 -> S
+    5 -> T
+    _ -> L
+
+
+sendNewPiece : Int -> Int -> Model -> Model
+sendNewPiece xPos shapeType model =
+  { model | shapes = { x = xPos, y = 0, shapeType = (decodeShapeType shapeType) } :: model.shapes }
 
 
 update msg model =
-  model
+  case msg of
+    Tick time ->
+      if model.pieceInterval >= 3 then
+        (model, Random.generate RandomXPos (Random.int 1 (board.width//board.tileSize)))
+      else
+        ({ model | pieceInterval = model.pieceInterval + 1 , shapes = List.map dropOnePixel model.shapes }, Cmd.none)
+    RandomXPos xPos ->
+      (model, Random.generate (RandomType xPos) (Random.int 1 5))
+    RandomType xPos shapeType ->
+      (sendNewPiece xPos shapeType { model | pieceInterval = 1, shapes = List.map dropOnePixel model.shapes }, Cmd.none)
+    EmptyMsg ->
+      (model, Cmd.none)
+      
 
 
-tetris : List Form
-tetris =
-  [ Shape.generateShape (2, 4) L
-  , Shape.generateShape (1, 7) S
-  , Grid.gridLines
-  ]
+tetris : Model -> List Form
+tetris model =
+  Grid.gridLines :: (List.map shapeToForm model.shapes)
 
 
-tetrisHtml : Html Msg
-tetrisHtml =
+tetrisHtml : Model -> Html Msg
+tetrisHtml model =
   collage 301 601
-    [ tetris |> group |> move (-150, 300)
+    [ tetris model |> group |> move (-150, 300)
     ] |> toHtml
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every (100 * millisecond) Tick
 
 
 view model =
   div []
-    [ tetrisHtml
+    [ tetrisHtml model
     ]
